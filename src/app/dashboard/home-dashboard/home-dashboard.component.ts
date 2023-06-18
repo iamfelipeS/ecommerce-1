e5c0c869fae1da64933f85f9784315e34638db1a
rafasdiass <128534833+rafasdiass@users.noreply.github.com>
2023-06-18 20:37:57 -0300

    alteracoes em produtos

--- src/app/dashboard/home-dashboard/home-dashboard.component.ts
+++ src/app/dashboard/home-dashboard/home-dashboard.component.ts
@@ -6,5 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-dashboard.component.scss'],
 })
 export class HomeDashboardComponent {}
-// felipe é o cara dos botoes!
-//felipe styless!!
+
